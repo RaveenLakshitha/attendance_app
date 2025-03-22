9edfd93104d9e90f149a1772aa2e3dabb98bd75a
Raveen_lw <raveenlw44@gmail.com>
2025-03-22 15:24:35 +0530

    first commit-10

--- database/migrations/2025_03_18_144731_create_attendances_table.php
+++ database/migrations/2025_03_18_144731_create_attendances_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
 {
     Schema::create('attendances', function (Blueprint $table) {
         $table->id();
+        $table->foreignId('user_id');
         $table->string('key')->unique();
         $table->text('value')->nullable();
         $table->timestamps();
